eabdaf988dcfe1efe9c44bd2e3d3bf1cf0360ff9
Jose Estrada <jose.estrada@foodcoin.global>
2022-05-01 05:29:00 +0200

    first commit

--- index.js
+++ index.js
@@ -2,21 +2,36 @@ const express = require('express');
 const path = require('path');
 require('dotenv').config();
 
+
+// DB congif
+require('./database/config').dbConnection();
+
 // App de Express
 const app = express();
 
-// Node server
+// Lectura y parseo del body
+app.use(express.json());
+
+// Node Server
 const server = require('http').createServer(app);
 module.exports.io = require('socket.io')(server);
 require('./sockets/socket');
 
-//Path público
+
+// Path público
 const publicPath = path.resolve( __dirname, 'public' );
+app.use( express.static( publicPath ) );
 
-app.use(express.static( publicPath ));
 
-server.listen(process.env.PORT, (err) => {
-    if (err) throw new Error (err);
+// Mis Rutas
+app.use( '/api/login', require('./routes/auth') )
 
-    console.log('Servidor corriendo en puerto: ', process.env.PORT);
+
+server.listen( process.env.PORT, ( err ) => {
+
+    if ( err ) throw new Error(err);
+
+    console.log('Servidor corriendo en puerto', process.env.PORT );
+
 });
+
---
+++ routes/auth.js
@@ -0,0 +1,27 @@
+/*
+    path: api/login
+*/
+const {Router} = require('express');
+const { check } = require('express-validator');
+const { crearUsuario, login, renewToken } = require('../controllers/auth');
+const { validarCampos } = require('../middlewares/validar-campos');
+const { validarJWT } = require('../middlewares/validar-jwt');
+
+const router = Router();
+
+router.post('/new', [
+    check('nombre', 'El nombre es olbligatorio').not().isEmpty(),
+    check('email', 'El email es olbligatorio').isEmail(),
+    check('password', 'La contraseña es olbligatorio').not().isEmpty(),
+    validarCampos
+] ,crearUsuario);
+
+router.post('/', [
+    check('email', 'El email es olbligatorio').isEmail(),
+    check('password', 'La contraseña es olbligatorio').not().isEmpty(),
+    validarCampos
+] ,login);
+
+router.get('/renew',validarJWT, renewToken);
+
+module.exports = router;
