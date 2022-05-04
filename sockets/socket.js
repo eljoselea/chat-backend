a7448f7a9613d9212bb655efdae217fcf8e77436
Jose Estrada <jose.estrada@foodcoin.global>
2022-05-04 04:38:30 +0200

    Fin sec 10 - backend

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,12 +1,42 @@
+const { comprobarJWT } = require('../helpers/jwt');
 const {io} = require('../index');
-
+const {usuarioConectado, usuarioDesconectado, grabarMensaje} = require ('../controllers/socket')
 
 //Mensajes de sockets
-io.on('connection', client => {
+io.on('connection', (client) => {
     console.log('Cliente conectado');
+    //console.log(client.handshake.headers);
+
+    //?? cliente con jwt?
+    const [valido, uid] = comprobarJWT(client.handshake.headers['x-token'])
+
+    //console.log(valido, uid);
+
+    // Verificar autenticación
+    if (!valido) {return client.disconnect();}
+    
+    // Cliente autenticado
+        usuarioConectado(uid);
+
+    // Ingresar al usuario a una sala en particular
+    //sala global, client.id    626fedb6ac68b19dfc6b79b2
+    client.join(uid);
+
+    //Escuchar del cliente el mensaje-personal
+    client.on('mensaje-personal', async (payload) => {
+    //Grabar mensaje
+    await grabarMensaje(payload);
+
+
+        //console.log(payload);
+        io.to(payload.para).emit('mensaje-personal', payload);
+    })
+
+
 
     client.on('disconnect', () => { 
-        console.log ('Cliente desconectado')
+        //console.log ('Cliente desconectado')
+        usuarioDesconectado(uid);
     });
     
     //client.on('emitir-mensaje', (payload) => {
